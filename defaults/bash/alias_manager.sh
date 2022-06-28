3a355eec52128217e7c95fd731da222ac0efc3d8
ewsgit <ewsgit@duck.com>
2022-06-28 19:01:22 +0100

    git error

--- defaults/bash/alias_manager.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# EwsgitOS (c) 2022 Ewsgit
-
-#generate and set/remove aliases in aliases.sh
--- intergrate.sh
+++ intergrate.sh
@@ -2,11 +2,17 @@
 
 echo "configurating XDG variables..."
 
-sudo echo "" > /etc/profile
+sudo echo "#!/bin/bash\n" > /etc/profile.d/ewsgit_os_xdg.sh
+
+sudo echo 'export XDG_DATA_HOME="~/config/home"' >> /etc/profile.d/ewsgit_os_xdg.sh
+sudo echo 'export XDG_CONFIG_HOME="~/config/config"' >> /etc/profile.d/ewsgit_os_xdg.sh
+sudo echo 'export XDG_STATE_HOME="~/config/state"' >> /etc/profile.d/ewsgit_os_xdg.sh
+sudo echo 'export XDG_CACHE_HOME="~/config/cache"' >> /etc/profile.d/ewsgit_os_xdg.sh
+
 
 echo "intergrating defaults"
 
-mkdir ~/.config/
+cp ./defaults/default_home_dir/ -r ~
 
-cp ./defaults/nvim/ -r ~/.config/.nvim/
-cp ./default/bash -r ~/.config/.bash/
+cp ./defaults/nvim/ -r ~/config/config/.nvim/
+cp ./default/bash -r ~/config/config/.bash/
