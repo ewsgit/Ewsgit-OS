91519e42955d4ec236a4642e6faad03185581422
ewsgit <ewsgit@duck.com>
2022-07-03 10:44:14 +0100

    no more confirmation on installing with pacman

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 # EwsgitOS © 2022 Ewsgit
 # Licensed under the MIT license: https://ewsgit.mit-license.org
 
-pacman -Syy -v
-pacman -S git -v
+pacman -Syy -v --noconfirm
+pacman -S git -v --noconfirm
 git clone https://github.com/ewsgit/Ewsgit-OS/tree/installation-testing/
 cd installation-testing
 cd installation
