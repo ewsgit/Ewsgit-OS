141878c31a62a632a194ae478b81c940a9671e66
ewsgit <ewsgit@duck.com>
2022-07-18 16:50:18 +0100

    begin to add desktop environment installation

--- intergrate.sh
+++ intergrate.sh
@@ -1,5 +1,12 @@
 !/bin/bash
 
+echo "     /\\      "
+echo "    /  \\     "
+echo "   / /\\ \\   "
+echo "  / /  \\ \\ "
+echo " /  ----  \\   "
+echo " ==     ==     "
+
 echo "configurating XDG variables..."
 
 sudo echo "#!/bin/bash\n" > /etc/profile.d/ewsgit_os_xdg.sh
@@ -9,10 +16,34 @@ sudo echo 'export XDG_CONFIG_HOME="~/config/config"' >> /etc/profile.d/ewsgit_os
 sudo echo 'export XDG_STATE_HOME="~/config/state"' >> /etc/profile.d/ewsgit_os_xdg.sh
 sudo echo 'export XDG_CACHE_HOME="~/config/cache"' >> /etc/profile.d/ewsgit_os_xdg.sh
 
+echo "installing yay (aur helper)"
 
 echo "intergrating defaults"
 
-cp ./defaults/default_home_dir/ -r ~
+sudo rm ~/
+mkdir ~/projects
+mkdir ~/downloads
+mkdir ~/desktop
+mkdir ~/config
+mkdir ~/config/path
+mkdir ~/config/config
+mkdir ~/config/state
+mkdir ~/config/cache
+mkdir ~/media
+mkdir ~/remote
 
-cp ./defaults/nvim/ -r ~/config/config/.nvim/
+cp ./defaults/nvim -r ~/config/config/.nvim/
 cp ./default/bash -r ~/config/config/.bash/
+cp ./defaults/alacritty -r ~/config/config/alacritty
+
+echo "select a desktop environment to install."
+select DESKTOP_ENV in "gnome" "kde" "i3" "continue";
+do
+    if [$DESKTOP_ENV == "continue"]; then;
+        echo "The system should reboot shortly"
+        break;
+    fi
+    if [$DESKTOP_ENV == "gnome"]; then;
+        echo "Installing the Gnome Desktop Environment"
+    fi
+done
