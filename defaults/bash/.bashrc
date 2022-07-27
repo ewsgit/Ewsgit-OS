49428f7e6e5ae181dbe3fa59414edb8d5569a386
ewsgit <ewsgit@duck.com>
2022-07-27 07:15:25 +0100

    fix bash powerline and first working installation (i3) also added the bash startup_banner

--- defaults/bash/.bashrc
+++ defaults/bash/.bashrc
@@ -10,16 +10,12 @@ shopt -s histappend
 HISTSIZE=200
 HISTFILESIZE=200
 shopt -s checkwinsize
-
-PS1='\e[m[\e[0;31m\u\e[m@\h] \e[0;32m\w \$\e[m '
-
-     alias ls='ls --color=auto'
-     alias dir='dir --color=auto'
-     alias vdir='vdir --color=auto'
-
-     alias grep='grep --color=auto'
-     alias fgrep='fgrep --color=auto'
-     alias egrep='egrep --color=auto'
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
@@ -44,15 +40,15 @@ alias vim="nvim"
 alias edit="nvim"
 alias ed="nvim"
 alias su="sudo su -p"
-alias ls="exa"
-alias l="ls"
-alias ll="ls"
+alias ls="exa -la"
+alias l="ls -la"
+alias ll="ls -la"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-EDITOR=nvim
+export EDITOR=nvim
 
 export PATH="~/Path:$PATH"
 PATH=~/.local/bin/:$PATH
--- intergrate.sh
+++ intergrate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-USER_DIR=$PWD
+USER_DIR=/home/ewsgit
+USER_NAME=ewsgit
 
 if (("$EUID" != 0)); then
     if [[ -t 1 ]]; then
@@ -17,17 +18,15 @@ echo "Beginning install"
 
 echo "configurating XDG variables..."
 
-sudo echo "#!/bin/bash" > /etc/profile.d/ewsgit_os_xdg.sh
-
-sudo echo 'export XDG_DATA_HOME="$HOME/config/local/share/"' >> /etc/profile.d/ewsgit_os_xdg.sh
-sudo echo 'export XDG_CONFIG_HOME="$HOME/config/config"' >> /etc/profile.d/ewsgit_os_xdg.sh
-sudo echo 'export XDG_STATE_HOME="$HOME/config/local/state"' >> /etc/profile.d/ewsgit_os_xdg.sh
-sudo echo 'export XDG_CACHE_HOME="$HOME/config/cache"' >> /etc/profile.d/ewsgit_os_xdg.sh
+echo "#!/bin/bash" > /etc/profile.d/ewsgit_os_xdg.sh
+echo 'export XDG_DATA_HOME="$HOME/config/local/share/"' >> /etc/profile.d/ewsgit_os_xdg.sh
+echo 'export XDG_CONFIG_HOME="$HOME/config/config"' >> /etc/profile.d/ewsgit_os_xdg.sh
+echo 'export XDG_STATE_HOME="$HOME/config/local/state"' >> /etc/profile.d/ewsgit_os_xdg.sh
+echo 'export XDG_CACHE_HOME="$HOME/config/cache"' >> /etc/profile.d/ewsgit_os_xdg.sh
 
 echo "intergrating defaults"
 
-mv $USER_DIR $USER_DIR/pre_ewsgit-os_backup/
-sudo rm ~/ -r
+mv $USER_DIR/* $USER_DIR/pre_ewsgit-os_backup/
 
 mkdir $USER_DIR/projects
 mkdir $USER_DIR/downloads
@@ -59,90 +58,78 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "Syncing Repositories"
-sudo pacman -Syy --no-confirm
+sudo pacman -Syy --noconfirm
 echo "Installing Git"
-sudo pacman -S git --no-confirm
+sudo pacman -S git --noconfirm
 echo "installing yay (aur helper)"
 sudo git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
-makepkg -si
+sudo -u $USER_NAME makepkg -si
 
-yay -S alacritty neovim xorg --answerdiff=None --noconfirm
+sudo -u $USER_NAME yay -S alacritty neovim xorg --answerdiff=None --noconfirm
+echo "installing the 'Jetbrains Mono typeface'"
+sudo -U $USER_NAME yay -S ttf-jetbrains-mono --answerdiff=None --noconfirm
+sudo -u $USER_NAME yay -S dkms linux-headers --answerdiff=None --noconfirm
 
 echo "select a desktop environment to install."
-select DESKTOP_ENV in "gnome" "kde" "i3" "continue";
-do
-    if [$DESKTOP_ENV == "continue"]; then;
-	echo "==========="
-	echo "  WARNING  "
-	echo "==========="
-	echo "No Desktop Environment will be installed!"
-        echo "The system will reboot shortly"
-        break;
-    fi
-    if [$DESKTOP_ENV == "gnome"]; then;
-        echo "Installing the Gnome Desktop Environment"
-        yay -S gdm --answerdiff=None --noconfirm
-        sudo systemctl enable gdm.service
-        yay -S gnome --noconfirm --answerdiff=None
-	break;
-    fi
-    if [$DESKTOP_ENV == "kde"]; then;
-        echo "Installing the Kde Desktop Environment"
-        yay -S plasma plasma-wayland-session kde-applications --answerdiff=None --noconfirm
-        sudo systemctl enable sddm.service
-        sudo systemctl enable NetworkManager.service
-        echo "Installing FireFox"
-        yay -S firefox --noconfirm --answerdiff=None
-    	break;
-    fi
-    if [$DESKTOP_ENV == "kde"]; then;
-        echo "Installing the Kde Desktop Environment"
-        yay -S plasma plasma-wayland-session kde-applications
-        sudo systemctl enable sddm.service
-        sudo systemctl enable NetworkManager.service
-        echo "Installing FireFox"
-        yay -S firefox
-    	break;
-    fi
-    if [$DESKTOP_ENV == "i3"]; then;
-	echo "Installing sddm login manager"
-	yay -S sddm --answerdiff=None --noconfirm
-	sudo systemctl enable sddm
-        echo "Installing the i3 Window Manager"
-        yay -S i3-gaps --answerdiff=None --noconfirm
-        echo "installing NetworkManager"
-        yay -S NetworkManager --answerdiff=None --noconfirm
-        sudo systemctl disable iwd
-        sudo systemctl stop iwd
-        sudo systemctl enable NetworkManager
-        sudo systemctl start NetworkManager
-        echo "installing default i3 configuration files"
-        cp ./defaults/i3 ~/config/config/i3
-        cp ./defaults/i3status ~/config/config/i3
-        echo "installing default applications"
-        echo "installing alacritty"
-	yay -S alacritty --answerdiff=None --noconfirm
-	echo "installing FireFox"
-        yay -S firefox --answerdiff=None --noconfirm
-        echo "installing picom"
-        yay -S picom --answerdiff=None --noconfirm
-        echo "installing dmenu"
-        yay -S dmenu --answerdiff=None --noconfirm
-        yay -S feh --answerdiff=None --noconfirm
-        echo "installing NetworkManager Gui and Cli"
-        yay -S nmcli --answerdiff=None --noconfirm
-        yay -S nm-applet --answerdiff=None --noconfirm
-	echo "installing capnet-assistant (manage and display portal wifi login systems)"
-	yay -S capnet-assistant
-        echo "removing iwd"
-        yay -R iwd --answerdiff=None --noconfirm
-        yay -R iwctl --answerdiff=None --noconfirm
-    	break;
-        echo "removing iwd"
-        yay -R iwd --answerdiff=None --noconfirm
-        yay -R iwctl --answerdiff=None --noconfirm
-    fi
+select DESKTOP_ENV in "gnome" "kde" "i3" "continue"; do
+	case $DESKTOP_ENV in
+		continue)
+			echo "==========="
+			echo "  WARNING  "
+			echo "==========="
+			echo "No Desktop Environment will be installed!"
+        		echo "The system will reboot shortly"
+        	;;
+		gnome)
+        		echo "Installing the Gnome Desktop Environment"
+        		sudo -u $USER_NAME yay -S gdm --answerdiff=None --noconfirm
+        		systemctl enable gdm.service
+        		sudo -u $USER_NAME yay -S gnome --noconfirm --answerdiff=None
+			sudo -u yay -S firefox --noconfirm --answerdiff=None
+		;;
+    		kde)
+			echo "Installing the K Desktop Environment (kde)"
+        		sudo -u $USER_NAME yay -S plasma plasma-wayland-session kde-applications --answerdiff=None --noconfirm
+        		systemctl enable sddm.service
+        		systemctl enable NetworkManager.service
+        		echo "Installing FireFox"
+        		sudo -u $USER_NAME yay -S firefox --noconfirm --answerdiff=None
+    		;;
+    		i3)
+			echo "Installing sddm login manager"
+			sudo -u $USER_NAME yay -S sddm --answerdiff=None --noconfirm
+			systemctl enable sddm
+		        echo "Installing the i3 Window Manager"
+	        	sudo -u $USER_NAME yay -S i3-gaps i3status --answerdiff=None --noconfirm
+		        echo "installing NetworkManager"
+	        	sudo -u $USER_NAME yay -S NetworkManager --answerdiff=None --noconfirm
+       			systemctl disable iwd
+        		systemctl stop iwd
+	        	systemctl enable NetworkManager
+		        systemctl start NetworkManager
+        		echo "installing default i3 configuration files"
+		        cp ./defaults/i3 ~/config/config/i3
+        		cp ./defaults/i3status ~/config/config/i3
+		        echo "installing default applications"
+       	 		echo "installing alacritty"
+			sudo -u $USER_NAME yay -S alacritty --answerdiff=None --noconfirm
+			echo "installing FireFox"
+	        	sudo -u $USER_NAME yay -S firefox --answerdiff=None --noconfirm
+        		echo "installing picom"
+			sudo -u $USER_NAME yay -S picom --answerdiff=None --noconfirm
+        		echo "installing dmenu"
+	        	sudo -u $USER_NAME yay -S dmenu --answerdiff=None --noconfirm
+			sudo -u $USER_NAME yay -S feh --answerdiff=None --noconfirm
+	        	echo "installing NetworkManager Gui and Cli"
+	        	sudo -u $USER_NAME yay -S nm-applet --answerdiff=None --noconfirm
+			echo "installing capnet-assistant (manage and display portal wifi login systems)"
+			sudo -u $USER_NAME yay -S capnet-assist --answerdiff=None --noconfirm
+        		echo "removing iwd"
+        		sudo -u $USER_NAME yay -R iwd --answerdiff=None --noconfirm
+        		sudo -u $USER_NAME yay -R iwctl --answerdiff=None --noconfirm
+		;;
+	esac
 done
 
 yay -Syu
